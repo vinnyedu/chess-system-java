5dfb96e8489e0cab49716fa907bf025388acd2f5
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-11-15 16:07:35 -0300

    add class: board, piece, position

---
+++ src/main/java/org/chess/application/Program.java
@@ -0,0 +1,11 @@
+package org.chess.application;
+
+import org.chess.boardgame.Board;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Board board = new Board(8, 8);
+
+    }
+}
