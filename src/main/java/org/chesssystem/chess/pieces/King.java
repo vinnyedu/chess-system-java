3b2e6a9720a86444a598e8fdb71fa8a17168eab5
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-11-20 11:42:25 -0300

    implementing king moves

--- src/main/java/org/chesssystem/chess/pieces/King.java
+++ src/main/java/org/chesssystem/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package org.chesssystem.chess.pieces;
 
 import org.chesssystem.boardgame.Board;
+import org.chesssystem.boardgame.Position;
 import org.chesssystem.chess.ChessPiece;
 import org.chesssystem.chess.Color;
 
@@ -19,8 +20,27 @@ public class King extends ChessPiece {
         }
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0, 0);
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                p.setValues(position.getRow() - i, position.getColumn() - j);
+                if (i == 0 && j == 0) {
+                    mat[p.getRow()][p.getColumn()] = false;
+                } else if (getBoard().positionExists(p) && canMove(p)) {
+                    mat[p.getRow()][p.getColumn()] = true;
+                }
+            }
+        }
+
+        return mat;
     }
 }
